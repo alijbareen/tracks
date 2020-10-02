23aa6145a1098ac6c5ea872a77f17d843bd00abd
alijb <alijb@post.bgu.ac.il>
2020-10-02 20:53:19 +0300

    Linking the Appp to Axios to handel server connections

--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,24 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
 import { View, StyleSheet } from "react-native";
 import { Text, Input, Button } from "react-native-elements";
 import Spacer from "../components/Spacer";
 
 const SignupScreen = ({ navigation }) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
   return (
-    <>
+    <View style={styles.container}>
       <Spacer>
         <Text h3>Sign Up for Tracker</Text>
       </Spacer>
-      <Input label="Email" />
+      <Input
+        autoCapitalize="none"
+        autoCorrect={false}
+        label="Email"
+        value={email}
+        onChangeText={setEmail}
+      />
       <Spacer />
-      <Input label="Password" />
+      <Input
+        autoCapitalize="none"
+        autoCorrect={false}
+        secureTextEntry
+        label="Password"
+        value={password}
+        onChangeText={setPassword}
+      />
       <Spacer>
         <Button title="Sign Up" />
       </Spacer>
-    </>
+    </View>
   );
 };
 
-const styles = StyleSheet.create({});
+SignupScreen.navigationOptions = () => {
+  return {
+    header: null,
+  };
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    marginBottom: 150,
+  },
+});
 
 export default SignupScreen;
