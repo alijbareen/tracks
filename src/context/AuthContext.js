6069f7688677dd21de50d41428bf1461ce2219fe
alijb <alijb@post.bgu.ac.il>
2020-10-03 15:56:22 +0300

    Sign Out init

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -67,6 +67,6 @@ const signout = (dispatch) => {
 
 export const { Provider, Context } = createDataContext(
   authReducer,
-  { signin, signout, signup, clearErrorMessage },
+  { signin, signout, signup, clearErrorMessage, tryLocalSignin },
   { token: null, errorMessage: "" }
 );
