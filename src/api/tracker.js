450fd8370bd048d20cf8915009c59d695b6ede37
alijb <alijb@post.bgu.ac.il>
2020-10-03 19:38:14 +0300

    Fetching Created Tasks

--- src/api/tracker.js
+++ src/api/tracker.js
@@ -1,13 +1,13 @@
-import axios from 'axios';
-import { AsyncStorage } from 'react-native';
+import axios from "axios";
+import { AsyncStorage } from "react-native";
 
 const instance = axios.create({
-  baseURL: 'http://5fa02756d31c.ngrok.io',
+  baseURL: "http://48db4ca2c1dd.ngrok.io",
 });
 
 instance.interceptors.request.use(
   async (config) => {
-    const token = await AsyncStorage.getItem('token');
+    const token = await AsyncStorage.getItem("token");
     if (token) {
       config.headers.Authorization = `Bearer ${token}`;
     }
